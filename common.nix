4250bd453050e595176b0a7ca9dc0dd9883c88f1
Pierre Carrier <pc@rrier.fr>
2025-09-01 14:21:30 +0000

    who let the dogs down

--- common.nix
+++ common.nix
@@ -30,38 +30,38 @@ in
     };
     distributedBuilds = true;
     buildMachines = [
-      {
-        hostName = "gorilla";
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-        ];
-        protocol = "ssh-ng";
-        maxJobs = 16;
-        speedFactor = 1;
-        supportedFeatures = [
-          "nixos-test"
-          "benchmark"
-          "big-parallel"
-          "kvm"
-        ];
-      }
-      {
-        hostName = "dog";
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-        ];
-        protocol = "ssh-ng";
-        maxJobs = 16;
-        speedFactor = 2;
-        supportedFeatures = [
-          "nixos-test"
-          "benchmark"
-          "big-parallel"
-          "kvm"
-        ];
-      }
+      # {
+      #   hostName = "gorilla";
+      #   systems = [
+      #     "x86_64-linux"
+      #     "aarch64-linux"
+      #   ];
+      #   protocol = "ssh-ng";
+      #   maxJobs = 16;
+      #   speedFactor = 1;
+      #   supportedFeatures = [
+      #     "nixos-test"
+      #     "benchmark"
+      #     "big-parallel"
+      #     "kvm"
+      #   ];
+      # }
+      # {
+      #   hostName = "dog";
+      #   systems = [
+      #     "x86_64-linux"
+      #     "aarch64-linux"
+      #   ];
+      #   protocol = "ssh-ng";
+      #   maxJobs = 16;
+      #   speedFactor = 2;
+      #   supportedFeatures = [
+      #     "nixos-test"
+      #     "benchmark"
+      #     "big-parallel"
+      #     "kvm"
+      #   ];
+      # }
     ];
   };
   nixpkgs.config.allowUnfree = true;
