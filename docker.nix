23ca1f49421a0911de698f456da8bf2a80930752
Pierre Carrier <pc@rrier.fr>
2025-09-01 22:44:46 +0000

    insecure registry for minikube

--- docker.nix
+++ docker.nix
@@ -1,4 +1,7 @@
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    extraOptions = "--insecure-registry 10.42.42.42:5000";
+  };
   users.users.pcarrier.extraGroups = [ "docker" ];
 }
