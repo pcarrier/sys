a861a0af1985067abedb73855fb87191f66c7a4b
Pierre Carrier <pc@rrier.fr>
2025-09-20 19:31:46 +0000

    fwupd on bare

--- bare.nix
+++ bare.nix
@@ -3,7 +3,10 @@
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
-  services.tailscale.enable = true;
+  services = {
+    tailscale.enable = true;
+    fwupd.enable = true;
+  };
   networking.firewall.interfaces.tailscale0 = {
     allowedTCPPortRanges = [
       {
