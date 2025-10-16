fa0d03b25af1f32d967234a66794a5d201bdd131
Pierre Carrier <pc@rrier.fr>
2025-10-16 09:58:13 +0000

    hound

---
+++ hw/tower.nix
@@ -0,0 +1,57 @@
+{
+  boot = {
+    initrd.availableKernelModules = [
+      "nvme"
+      "xhci_pci"
+      "ahci"
+      "usbhid"
+    ];
+  };
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/root";
+      fsType = "btrfs";
+      options = [
+        "noatime"
+        "compress=zstd"
+        "ssd"
+      ];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
+    };
+  };
+  networking.networkmanager.enable = true;
+  programs.niri.enable = true;
+  security.polkit.enable = true;
+  hardware = {
+    bluetooth.enable = true;
+    cpu.amd.updateMicrocode = true;
+    graphics.enable = true;
+    nvidia.open = true;
+  };
+  services = {
+    logind = {
+      powerKey = "ignore";
+      suspendKey = "ignore";
+      hibernateKey = "ignore";
+      lidSwitch = "ignore";
+    };
+    greetd = {
+      enable = true;
+      settings = rec {
+        initial_session = {
+          command = "niri-session";
+          user = "pcarrier";
+        };
+        default_session = initial_session;
+      };
+    };
+    automatic-timezoned.enable = true;
+  };
+}
