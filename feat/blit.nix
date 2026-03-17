f0ba303f4b3c608b6f46fac632df682dc025ee87
Pierre Carrier <pc@rrier.fr>
2026-03-17 14:04:48 -0700

    superblit

--- feat/blit.nix
+++ feat/blit.nix
@@ -1,11 +1,27 @@
 { blit, system, ... }:
+let
+  pkg = blit.packages.${system}.default;
+in
 {
-  systemd.services.blit = {
-    description = "blit";
+  systemd.services.blit-gateway = {
+    description = "blit gateway";
+    after = [ "network.target" "blit-server.service" ];
+    requires = [ "blit-server.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkg}/bin/blit-gateway";
+      Restart = "on-failure";
+      User = "pcarrier";
+      EnvironmentFile = "/etc/blit-gateway.env";
+    };
+  };
+
+  systemd.services.blit-server = {
+    description = "blit server";
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${blit.packages.${system}.default}/bin/blit-server";
+      ExecStart = "${pkg}/bin/blit-server";
       Restart = "on-failure";
       User = "pcarrier";
       WorkingDirectory = "~";
