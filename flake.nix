0183928f36a28db771080ab6065fb1aff678ad55
Pierre Carrier <pc@rrier.fr>
2026-03-31 18:37:43 +0200

    blit v0.13.1

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     blit = {
-      url = "github:indent-com/blit";
+      url = "github:indent-com/blit/v0.13.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plenty = {
