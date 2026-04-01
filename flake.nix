7455a115227502b4dd16512061950461c38e2b80
Pierre Carrier <pc@rrier.fr>
2026-04-01 02:26:44 +0200

    blit forward

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     blit = {
-      url = "github:indent-com/blit/v0.13.1";
+      url = "github:indent-com/blit";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plenty = {
