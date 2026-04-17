baf062703e559ee5961c1ab2123d5339de032133
Pierre Carrier <pc@rrier.fr>
2026-04-18 00:53:08 +0200

    dovecot update

--- feat/mail.nix
+++ feat/mail.nix
@@ -1,7 +1,12 @@
 {
   services.dovecot2 = {
     enable = true;
-    enableImap = true;
-    mailLocation = "maildir:~/Maildir";
+    settings = {
+      dovecot_config_version = "2.4.3";
+      dovecot_storage_version = "2.4.3";
+      protocols.imap = true;
+      mail_driver = "maildir";
+      mail_path = "~/Maildir";
+    };
   };
 }
