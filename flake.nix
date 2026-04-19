dd569359804479d0530013faacf9235583f81b55
Pierre Carrier <pc@rrier.fr>
2026-04-19 19:11:51 +0000

    trust rabbit, blit 0.26.0

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     blit = {
-      url = "github:indent-com/blit/cpu_opt";
+      url = "github:indent-com/blit";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plenty = {
