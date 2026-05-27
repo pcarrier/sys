8c7066294a13adc30b4b116895196639c528a445
Pierre Carrier <pc@rrier.fr>
2026-05-26 20:31:33 -0700

    tailscale and blit on indentbox

--- hosts/indentbox.nix
+++ hosts/indentbox.nix
@@ -4,5 +4,50 @@ lib.ec2 {
   system = "aarch64-linux";
   extraModules = [
     ../feat/indentmoo.nix
+    (
+      { blit, ... }:
+      {
+        imports = [ blit.nixosModules.blit ];
+        services = {
+          blit = {
+            enable = true;
+            users = [ "pcarrier" ];
+            gateways.pcarrier = {
+              user = "pcarrier";
+              port = 3264;
+              passFile = "/etc/blit.env";
+            };
+          };
+          nginx.virtualHosts."blit.pierre.dev.indent.sh" = {
+            enableACME = true;
+            forceSSL = true;
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:3264/";
+              proxyWebsockets = true;
+              extraConfig = ''
+                proxy_buffering off;
+                proxy_request_buffering off;
+                tcp_nodelay on;
+              '';
+            };
+          };
+          tailscale.enable = true;
+        };
+        networking.firewall.interfaces.tailscale0 = {
+          allowedTCPPortRanges = [
+            {
+              from = 0;
+              to = 65535;
+            }
+          ];
+          allowedUDPPortRanges = [
+            {
+              from = 0;
+              to = 65535;
+            }
+          ];
+        };
+      }
+    )
   ];
 } lib.commonInputs
